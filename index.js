5aee013b49c886ae057e275039536d09c45a41ab
luisfelipesilvasantos <luisflp.375@gmail.com>
2025-03-06 22:33:08 -0300

    Codigo deClassificador de nivel de Heroi

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 let nome = "Luis";
-console.log(nome);
+let xp = 100;
+let nivel = "";
+
+if(xp <= 1000){
+    nivel = "Ferro"
+    console.log("O Herói de nome " + nome + " está no nível " + nivel);
+}else if(xp >= 1001 && xp <= 2000){
+    nivel = "Bronze"
+    console.log("O Herói de nome " + nome + " está no nível " + nivel);
+}else if(xp >= 2001 && xp <= 5000){
+    nivel = "Prata"
+    console.log("O Herói de nome " + nome + " está no nível " + nivel);
+}else if(xp >= 5001 && xp <= 7000){
+    nivel = "Ouro"
+    console.log("O Herói de nome " + nome + " está no nível " + nivel);
+}else if(xp >= 7001 && xp <= 8000){
+    nivel = "Platina"
+    console.log("O Herói de nome " + nome + " está no nível " + nivel);
+}else if(xp >= 8001 && xp <= 9000){
+    nivel = "Ascendente"
+    console.log("O Herói de nome " + nome + " está no nível " + nivel);
+}else if(xp >= 9001 && xp <= 10000){
+    nivel = "Imortal"
+    console.log("O Herói de nome " + nome + " está no nível " + nivel);
+}else{
+    nivel = "Radiante"
+    console.log("O Herói de nome " + nome + " está no nível " + nivel);
+}
